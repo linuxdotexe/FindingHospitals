a7bb59e91e7c9e3e0ad8ac13a42964acba591451
sai nivas mangu <sainonbeat99@gmail.com>
2024-02-16 17:48:48 +0530

    add: testng runner class

---
+++ src/test/java/com/practo/testRunner/TestNGRunner.java
@@ -0,0 +1,21 @@
+package com.practo.testRunner;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features= {".//FeatureFiles/"}, 
+		glue = "com.practo.stepDefinitions", 
+		plugin = {"pretty","html:reports/report.html","rerun:target/rerun.txt",
+				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+		dryRun=false,
+		monochrome=true
+		)
+public class TestNGRunner extends AbstractTestNGCucumberTests{
+
+}
+
