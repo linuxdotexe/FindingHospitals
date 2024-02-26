70b7db324baee0aee6e1cd2987beb8caf302e8e0
sai nivas mangu <sainonbeat99@gmail.com>
2024-02-26 15:56:50 +0530

    upd: run all features

--- src/test/java/com/practo/testRunner/TestRunner.java
+++ src/test/java/com/practo/testRunner/TestRunner.java
@@ -8,7 +8,7 @@ import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(
 		
-		features= {".//FeatureFiles/homePage.feature"}, 
+		features= {".//FeatureFiles/"}, 
 		glue = "com.practo.stepDefinitions", 
 		plugin = {"pretty","html:reports/report.html"},
 		dryRun=false,
