e43ed414349664e35e4d149a7eaa5c032e07df26
sai nivas mangu <sainonbeat99@gmail.com>
2024-02-29 15:40:29 +0530

    fix: features to be executed

--- src/test/java/com/practo/testRunner/TestNGRunner.java
+++ src/test/java/com/practo/testRunner/TestNGRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
-		features= {".//FeatureFiles/homePage.feature"}, 
+		features= {".//FeatureFiles/"}, 
 		glue = "com.practo.stepDefinitions", 
 		plugin = {"pretty","html:reports/report.html","rerun:target/rerun.txt",
 				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
