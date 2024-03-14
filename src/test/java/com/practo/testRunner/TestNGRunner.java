923874fe768db2a9262b7fa36bc477d608efa59b
sai nivas mangu <sainonbeat99@gmail.com>
2024-03-14 18:02:21 +0530

    feat: run tests by tags

--- src/test/java/com/practo/testRunner/TestNGRunner.java
+++ src/test/java/com/practo/testRunner/TestNGRunner.java
@@ -5,7 +5,12 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
 		features= {".//FeatureFiles/"}, 
-		glue = "com.practo.stepDefinitions", 
+		glue = "com.practo.stepDefinitions",
+		/*
+		 * run scenarios tagged with @test - project description tasks
+		 * other available tags - @smoke, @regression, @sanity
+		 */
+		// tags = "@test", 
 		plugin = {"pretty","html:reports/report.html","rerun:target/rerun.txt",
 				"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
 		dryRun=false,
